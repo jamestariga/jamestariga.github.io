85a7cfefa11c04acbf89e5afd8c0990dacdaf064
jamestariga <jamestariga08@live.com>
2022-06-02 22:21:40 -0700

    ✨ Added backend skills

--- src/components/Skills/SkillsData.js
+++ src/components/Skills/SkillsData.js
@@ -1,15 +1,20 @@
 const SkillsData = [
-  { bgColor: '#61DBFB', title: 'React', completed: '80%' },
+  { bgColor: '#61DBFB', title: 'React', completed: '85%' },
   { bgColor: '#f0db4f', title: 'JavaScript', completed: '90%' },
+  { bgColor: '#007acc', title: 'TypeScript', completed: '60%' },
   { bgColor: '#e34c26', title: 'HTML', completed: '95%' },
   { bgColor: '#2965f1', title: 'CSS', completed: '85%' },
+  { bgColor: '#61DBFB', title: 'React-Native', completed: '70%' },
+  { bgColor: 'palevioletred', title: 'Styled-Components', completed: '90%' },
+  { bgColor: '#000', title: 'Git', completed: '75%' },
+  { bgColor: '#2965f1', title: 'TailwindCSS', completed: '80%' },
+  { bgColor: '#3C873A', title: 'MongoDB', completed: '60%' },
+  { bgColor: '#68A063', title: 'Node.JS', completed: '70%' },
+  { bgColor: '#303030', title: 'Express.JS', completed: '70%' },
   { bgColor: '#264de4', title: 'Java', completed: '70%' },
   { bgColor: '#FFD43B', title: 'Python', completed: '60%' },
-  { bgColor: 'palevioletred', title: 'Styled-Components', completed: '90%' },
-  { bgColor: '#000', title: 'Git', completed: '70%' },
   { bgColor: '#787CB5', title: 'PHP', completed: '45%' },
   { bgColor: '#f9dd94', title: 'UI Design', completed: '65%' },
-  { bgColor: '#2965f1', title: 'TailwindCSS', completed: '60%' },
 ]
 
 export default SkillsData
